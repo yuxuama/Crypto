60f0b4265550af520019d5c26c3ca6c6f92dff89
yuxuama <matthieulecomte@free.fr>
2019-11-05 17:59:33 +0100

    test1,2,1,2

--- cesar.py
+++ cesar.py
@@ -1,4 +1,8 @@
 from math import *
 
+"""Encoding a message with Cesar process"""
+
 def encrypt(chain):
-    key = int(input("Donne une clé de codage"))
+    key = int(input("Donne une clé de codage: "))
+    message = input("Donne le message que tu veux crypter: ")
+    
