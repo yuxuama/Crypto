a0440eb7325c0e88c657f31df06c187bbc174933
unknown <alexandrecornet2003@gmail.com>
2019-11-05 18:48:31 +0100

    Fonctionne mais pas les espaces

--- cesar.py
+++ cesar.py
@@ -1,4 +1,20 @@
 """
-Test lol mdr
+
+Cryptage avec Cesar
 
 """
+
+
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+motclear = input("Texte: ").lower()
+key = int(input("Decallage: "))
+motcrypt = ''
+
+
+for letters in motclear :
+    chiffre = alphabet.index(letters) + key
+    if chiffre > 26 :
+        chiffre -=26
+    motcrypt += alphabet[chiffre]
+print("Mot Crypt: ",motcrypt)
