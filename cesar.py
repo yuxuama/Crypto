fa93b7257091f3604e12295be857f8ca0d41eb70
yuxuama <matthieulecomte@free.fr>
2019-11-05 20:36:04 +0100

    Resolution of all the bug and issues

--- cesar.py
+++ cesar.py
@@ -1,39 +1,88 @@
 #Author: Yuxuama
+
+#declaration of dedicated alphabet
+
+lowercase = "abcdefghijklmnopqrstuvwxyz"
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+special_char = "!?:;.,'éèàçù"
+numbers = "0123456789"
+
+
 ##  Encoding a message with Cesar process  ##
 
 def cesar_encrypt():
+
+    # Asking for key
     key = int(input("Donne une clé de codage: "))
-    if not 0 < key < 26:                                                              #Check if is in the range for a Cesar encode
+    if not 0 < key < 26:      #Check if the key is in the range for a Cesar encode
         raise ValueError("La clé de codage doit être comprise entre 0 et 26")
+
+    #Asking for message
     message = input("Donne le message que tu veux crypter: ")
     crypted_message = ''
+
+    #Crypt th message
     for char in message:
-        ord_char = ord(char)
-        if ord_char != 32 or ord_char != 44:                                          #I have a problem here the 'or' simply doesn't work and I don't know why
-            crypted_message = crypted_message + chr((ord_char - 97 + key) % 26 + 97)  #Coding char, the method allows to be only in the alphabet
+        if char in lowercase:
+            new_index = (lowercase.index(char) + key) % 26
+            crypted_message += lowercase[new_index]
+        elif char in uppercase:
+            new_index = (uppercase.index(char) + key) % 26
+            crypted_message += uppercase[new_index]
+        elif char in special_char:
+            new_index = (special_char.index(char) + key) % 12
+            crypted_message += special_char[new_index]
+        elif char in numbers:
+            new_index = (numbers.index(char) + key) % 10
+            crypted_message += numbers[new_index]
         else:
-            crypted_message = crypted_message + char
+            crypted_message += char
     return crypted_message
 
 ## Computation of all solution for a given coded chain ##
 
 def cesar_bf(chain):
     decrypted_message = ''
-    for i in range(-25, 1):
+    for i in range(26):
         for char in chain:
-            if char != ' ':
-                decrypted_message = decrypted_message + chr((ord(char) + i -97) % 26 + 97)
+            if char in lowercase:
+                new_indew = (lowercase.index(char) - i) % 26
+                decrypted_message += lowercase[new_indew]
+            elif char in uppercase:
+                new_indew = (uppercase.index(char) - i) % 26
+                decrypted_message += uppercase[new_indew]
+            elif char in special_char:
+                new_indew = (special_char.index(char) - i) % 12
+                decrypted_message += special_char[new_indew]
+            elif char in numbers:
+                new_indew = (numbers.index(char) - i) % 10
+                decrypted_message += numbers[new_indew]
             else:
-                decrypted_message = decrypted_message + char
-        print(decrypted_message, "the key was: ", -i)
+                decrypted_message += char
+        print(decrypted_message, "    the key of encryption was: ", i)
         decrypted_message = ''
 
-message = cesar_encrypt()
-print(" ")
-print("######  Encoded message   ######")
-print(message)
-print("################################ ")
-print(" ")
-print("######  Decoded solution  ######")
-cesar_bf(message)
-print("################################")
+#Ask to user what he wants to do
+
+will = input("Que veux tu faire? ")
+if will == 'd':
+    message = input("Quel message veux tu décrypter")
+    print("######  Decoded solution  ######")
+    cesar_bf(message)
+    print("################################")
+elif will == 'c':
+    message = cesar_encrypt()
+    print(" ")
+    print("######  Encoded message   ######")
+    print(message)
+    print("################################ ")
+elif will == "c et d":
+    message = cesar_encrypt()
+    print(" ")
+    print("######  Encoded message   ######")
+    print(message)
+    print("################################ ")
+    print(" ")
+    print("######  Decoded solution  ######")
+    cesar_bf(message)
+    print("################################")
