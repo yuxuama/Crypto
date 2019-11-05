d03c8ad6d109496a16391aa2080fcf5e72736c57
unknown <alexandrecornet2003@gmail.com>
2019-11-05 19:06:21 +0100

    Marche pour les espaces mais pasd les majusqules et caractères spéciaux

--- cesar.py
+++ cesar.py
@@ -4,17 +4,32 @@ Cryptage avec Cesar
 
 """
 
-
-
 alphabet = "abcdefghijklmnopqrstuvwxyz"
-motclear = input("Texte: ").lower()
-key = int(input("Decallage: "))
-motcrypt = ''
+
+#fonction pour crypter
+def crypt():
+
+    motclear = input("Texte: ").lower()
+    key = int(input("Decallage: "))
+    motcrypt = ''
+
+    for letters in motclear :
+        if letters == " ":
+            motcrypt += ' '
+            continue
+        chiffre = alphabet.index(letters) + key
+        if chiffre > 26 :
+            chiffre -=26
+        motcrypt += alphabet[chiffre]
+    print("Mot Crypt: ",motcrypt)
 
 
-for letters in motclear :
-    chiffre = alphabet.index(letters) + key
-    if chiffre > 26 :
-        chiffre -=26
-    motcrypt += alphabet[chiffre]
-print("Mot Crypt: ",motcrypt)
+#terminal pour les commandes
+while True:
+    cmd = input(">>> ")
+    if cmd == 'crypt':
+        crypt()
+    elif cmd == 'stop':
+        break
+    else:
+        print("Command Invalide")
