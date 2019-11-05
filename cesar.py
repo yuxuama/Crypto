75bc4c050f2acd6eac8a9e65c880c5e5662e2fea
yuxuama <matthieulecomte@free.fr>
2019-11-05 18:07:42 +0100

    Cesar algorythm to encrypt chain messages

--- cesar.py
+++ cesar.py
@@ -1,8 +1,17 @@
-from math import *
 
 """Encoding a message with Cesar process"""
 
-def encrypt(chain):
+def encrypt():
     key = int(input("Donne une clé de codage: "))
+    if not 0 < key < 26:
+        raise ValueError("La clé de codage doit être comprise entre 0 et 26")
     message = input("Donne le message que tu veux crypter: ")
-    
+    crypted_message = ''
+    for char in message:
+        if char != ' ' or char != ',':
+            crypted_message = crypted_message + chr(ord(char) + key)
+        else:
+            crypted_message = crypted_message + char
+    return crypted_message
+
+print(encrypt())
