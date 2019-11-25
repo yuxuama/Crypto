4fb1c0593a7fc0b1990c7be2df1c6cb9335eda70
yuxuama <matthieulecomte@free.fr>
2019-11-25 19:15:52 +0100

    Final project finished (~)

--- Final Projet.py
+++ Final Projet.py
@@ -22,16 +22,16 @@ statistic_french_base_c = [['a','b','c','d','e','f','g','h','i','j','k','l','m',
 statistic_french_base_v = [['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'], \
                          [8.4, 1.06, 3.03, 4.18, 17.26, 1.12, 1.27, 0.92, 7.34, 0.31, 0.05, 6.01, 2.96, 7.13, 5.26, 3.01,0.99, 6.55, 8.08, 7.07, 5.74, 1.32, 0.04, 0.45, 0.3, 0.12]]
 
-#############################
-#### Interface Function  ####
-#############################
+####################################################
+####            Interface Function              ####
+####################################################
              
 def user_interface():
     # Main menu
     print("======  Main menu   ", "="*34)
-    print("1 Cesar method")
-    print("2 Vigenere method")
-    print("3 Beaufort method")
+    print("1 Cesar methods")
+    print("2 Vigenere methods")
+    print("3 Beaufort methods")
     print("4 Quit")
     print("="*55)
     cmd = int(input(">>> "))
@@ -48,10 +48,10 @@ def user_interface():
 def cesar_interface():
     #Display of the menu
     print("=" * 55)
-    print("1 Crypt", "             Crypt a message ")
-    print("2 Uncrypt", "           Uncrypt a message")
-    print("3 Bruteforce", "        Attack with brute force")
-    print("4 Crypto Analysis", "   Attack with statistic method")
+    print("1 Crypt", "              Crypt a message ")
+    print("2 Uncrypt", "            Uncrypt a message")
+    print("3 Bruteforce", "         Attack with brute force")
+    print("4 Crypto Analysis", "    Attack with statistic method")
     print("5 Return to main menu")
     print("6 Quit")
     print("=" * 55)
@@ -68,7 +68,7 @@ def cesar_interface():
             cesar_bf(infos)
         elif cmd == 4:
             infos = input("Crypted message: ")
-            cesar_cs(infos, 1)
+            crypto_analyse_stats(infos, 1, "c")
         elif cmd == 5:
             user_interface()
             break
@@ -100,9 +100,10 @@ def vigenere_interface():
             infos = ask("")
             print("Decrypted message: ", vigenere_uncrypt(infos[0], infos[1]))
         elif cmd == 3:
-            print("Undefined")
+            print("Undefined for now")
         elif cmd == 4:
-            print("Undefined")
+            info = input("Crypted message: ")
+            crypto_analyse_stats(info, 1, "v")
         elif cmd == 5:
             user_interface()
             break
@@ -115,6 +116,7 @@ def vigenere_interface():
   
 # Input method  
 def ask(method):
+    # Receive chain (method) -> it identifies rather cesar method or not
     while 1:
         message = input("Texte: ")
         key = input("Key: ")
@@ -135,14 +137,14 @@ def ask(method):
                 print("La clé ne doit pas contenir de chiffre")
                 continue
 
-################################
-### Cesar encryption method  ###   
-################################
+###############################################
+####        Cesar encryption methods       ####
+###############################################
 
 def cesar_encrypt(chain, key):
     crypted_message = ''
 
-    #Crypt th message
+    #Crypt the message
     for char in chain:
         if char in lowercase:
             new_index = (lowercase.index(char) + key) % 26
@@ -181,73 +183,17 @@ def cesar_uncrypt(chain, key):
     return decrypted_message
 
 def cesar_bf(chain):
-    decrypted_message = ''
     for i in range(1, 26):
         decrypted_message = cesar_uncrypt(chain, i)
         print(decrypted_message, "    the key of encryption was: {0}".format(i))
         
-def cesar_cs(chain, error):
-    #Transform character into lowercase
-    chain = chain.lower()
 
-    #Creation of work table (statistic table)
-    work_table = [["Charactères"], \
-                  ["Effectifs  "], \
-                  ["Fréquence  "], \
-                  ["Suggestion "]]
-    total_effectif = 0
 
-    #  Statistic Analysis  #
-    for char in chain:
-        if work_table[0].count(char) == 0:
-            if char != ' ' and not char in special_char:
-                work_table[0].append(char)
-                work_table[1].append(1)
-        else:
-            index = work_table[0].index(char)
-            work_table[1][index] += 1
-    #Computing frequences (via total effectif)
-    for i in range(2):
-        for number in work_table[1]:
-            if type(number) is not str:
-                if i == 0:
-                    total_effectif += number
-                else:
-                    work_table[2].append(round((number / total_effectif) * 100, 2))
 
-    #Compare with data base #return the value of database that fit the well to computing frequences, if there is not: return 'NA'
-    base = statistic_french_base_c
-    maxi_proche = [0, 'NA']   #Save var to chose the best char (see below)
-    for index in range(1, len(work_table[0])):
-        for base_i in range(len(base[0])):
-            if base[1][base_i] - error < work_table[2][index] < base[1][base_i] + error:    #testing if frequence value is in range
-                rapport = work_table[2][index]/base[1][base_i]    #Compute how close the two values are
-                sugested_char = base[0][base_i]
-                if maxi_proche[0] < rapport:  #If precedent save quotient is smaller, redefine the suggested char value
-                    maxi_proche[1] = sugested_char
-                maxi_proche[0] = max(maxi_proche[0], rapport)
-        work_table[3].append(maxi_proche[1])
-        maxi_proche = [0, 'NA']
 
-    #Display work_table with a clean print table
-    for i in range(4):
-        for j in range(len(work_table[0])):
-            char = work_table[i][j]
-            if type(char) is str:
-                lenght = len(char)
-                if lenght < 5:
-                    char += ' '*(5-lenght)
-            elif type(char) is int or type(char) is float:
-                char = str(char)
-                lenght = len(char)
-                if lenght < 5:
-                    char += ' ' * (5 - lenght)
-            print("|", char, end='')
-        print('||')
-
-################################
-####    Vigenere methods    ####
-################################
+###################################################
+####             Vigenere methods              ####
+###################################################
 
 def vigenere_encrypt(mot, key):
     cmpt = 0
@@ -291,5 +237,74 @@ def vigenere_uncrypt(mot, key):
             cmpt = (cmpt + 1) % len(key)
 
     return motcrypt
-    
+
+###########################################
+####      Cryptanalyse statistique     ####
+###########################################
+
+def crypto_analyse_stats(chain, error, method):
+    #Choose the database in function of the method used
+    if method == "c":
+        base = statistic_french_base_c
+    else:
+        base = statistic_french_base_v
+
+    #Transform character into lowercase
+    chain = chain.lower()
+
+    #Creation of work table (statistic table)
+    work_table = [["Charactères"], \
+                  ["Effectifs  "], \
+                  ["Fréquence  "], \
+                  ["Suggestion "]]
+    total_effectif = 0
+
+    #  Statistic Analysis  #
+    for char in chain:
+        if work_table[0].count(char) == 0:
+            if char != ' ' and not char in special_char:
+                work_table[0].append(char)
+                work_table[1].append(1)
+        else:
+            index = work_table[0].index(char)
+            work_table[1][index] += 1
+    #Computing frequences (via total effectif)
+    for i in range(2):
+        for number in work_table[1]:
+            if type(number) is not str:
+                if i == 0:
+                    total_effectif += number
+                else:
+                    work_table[2].append(round((number / total_effectif) * 100, 2))
+
+    #Compare with data base #return the value of database that fit the well to computing frequences, if there is not: return 'NA'
+    maxi_proche = [0, 'NA']   #Save var to choose the best char (see below)
+    for index in range(1, len(work_table[0])):
+        for base_i in range(len(base[0])):
+            if base[1][base_i] - error < work_table[2][index] < base[1][base_i] + error:    #testing if frequence value is in range
+                rapport = work_table[2][index]/base[1][base_i]    #Compute how close the two values are
+                sugested_char = base[0][base_i]
+                if maxi_proche[0] < rapport:  #If precedent save quotient is smaller, redefine the suggested char value
+                    maxi_proche[1] = sugested_char
+                maxi_proche[0] = max(maxi_proche[0], rapport)
+        work_table[3].append(maxi_proche[1])
+        maxi_proche = [0, 'NA']
+
+    # Display work_table with a clean print table
+    for i in range(4):
+        for j in range(len(work_table[0])):
+            char = work_table[i][j]
+            if type(char) is str:
+                lenght = len(char)
+                if lenght < 5:
+                    char += ' ' * (5 - lenght)
+            elif type(char) is int or type(char) is float:
+                char = str(char)
+                lenght = len(char)
+                if lenght < 5:
+                    char += ' ' * (5 - lenght)
+            print("|", char, end='')
+        print('||')
+
+# Run
 user_interface()
